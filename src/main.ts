8aef833a6a2afa5ee5f0d0b565b884a8f2e6c5d4
User <user@example.com>
2026-05-19 15:57:37 +0800

    feat: 引入 Element Plus 组件库

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createSSRApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import './styles/iconfont.css'
 
@@ -7,5 +9,6 @@ export function createApp() {
   const app = createSSRApp(App)
   const pinia = createPinia()
   app.use(pinia)
+  app.use(ElementPlus)
   return { app }
 }
